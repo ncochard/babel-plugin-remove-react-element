ba3ed97ae8754142360d451bf736f978a7572e7c
Nicolas Cochard <nicolas@cochard.co.uk>
2019-12-30 19:56:51 +0000

    Fixed the unit tests

--- test/fixtures/basic/output.js
+++ test/fixtures/basic/output.js
@@ -1,37 +1,37 @@
-import MediaQuery from 'react-responsive';
-import React, { Component } from 'react';
+import MediaQuery from "react-responsive";
+import React, { Component } from "react";
 
 class Desktop extends Component {
-    render() {
-        return React.createElement(
-            MediaQuery,
-            { query: '(min-device-width: 1224px)' },
-            this.props.children
-        );
-    }
+  render() {
+    return React.createElement(
+      MediaQuery,
+      {
+        query: "(min-device-width: 1224px)"
+      },
+      this.props.children
+    );
+  }
 }
 
 class Mobile extends Component {
-    render() {
-        return React.createElement(
-            MediaQuery,
-            { query: '(max-device-width: 1224px)' },
-            this.props.children
-        );
-    }
+  render() {
+    return React.createElement(
+      MediaQuery,
+      {
+        query: "(max-device-width: 1224px)"
+      },
+      this.props.children
+    );
+  }
 }
 
 export class MyPage extends Component {
-    render() {
-        return React.createElement(
-            'div',
-            null,
-            null,
-            React.createElement(
-                Mobile,
-                null,
-                'This will show on mobile.'
-            )
-        );
-    }
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      null,
+      React.createElement(Mobile, null, "This will show on mobile.")
+    );
+  }
 }
